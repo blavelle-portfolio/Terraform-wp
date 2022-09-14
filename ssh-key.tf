599b43d13722cbe1df341955e41f7073cfd3358e
blavelle-portfolio <brendan.lavelle@slalom.com>
2022-09-14 14:01:38 -0400

    updating permissions

--- ssh-key.tf
+++ ssh-key.tf
@@ -1,20 +1,10 @@
-resource "tls_private_key" "pk" {
+resource "tls_private_key" "main" {
   algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "kp" {
-  key_name   = "myKey"       # Create "myKey" to AWS!!
-  public_key = tls_private_key.pk.public_key_openssh
-
-  provisioner "local-exec" { # Create "myKey.pem" to your computer!!
-    command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
-  }
 }
 
 resource "null_resource" "main" {
   provisioner "local-exec" {
-    command = "echo \"${tls_private_key.main.private_key_pem}\" > private.pem"
+    command = "sudo echo \"${tls_private_key.main.private_key_pem}\" >> $PWD/private.pem"
   }
 
   provisioner "local-exec" {
